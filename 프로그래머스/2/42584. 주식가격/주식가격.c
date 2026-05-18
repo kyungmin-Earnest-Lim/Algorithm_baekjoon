f512b5daaff13dc0b4c009c9dc3410db9ea4fbbb
kyungminLim <65112294+kyungmin-Earnest-Lim@users.noreply.github.com>
2026-05-18 22:23:16 +0900

    [level 2] Title: 주식가격, Time: 20.94 ms, Memory: 27.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// prices_len은 배열 prices의 길이입니다.
+int* solution(int prices[], size_t prices_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(prices_len*sizeof(int));
+    for(size_t i=0; i<prices_len; i++) {
+        answer[i] = prices_len - i - 1;
+        for(size_t j=i+1; j<prices_len; j++) {
+            if (prices[i] > prices[j]) {
+                answer[i] = j - i;
+                break;
+            }
+        }
+    }
+    
+    return answer;
+}
