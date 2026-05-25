cc1faed0ee3b0d536ee2c904212b5dc6152c5289
kyungminLim <65112294+kyungmin-Earnest-Lim@users.noreply.github.com>
2026-05-25 14:45:59 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.02 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.c
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.c
@@ -4,12 +4,12 @@
 
 int* solution(long long n) {
     // 리턴할 값은 메모리를 동적 할당해주세요.
-    int len=0;
-    for (long long i=n; i!=0; i/=10) {
-        len++;
-    }
-    int* answer = (int*)malloc(sizeof(int) * len);
-    for (int i=0; i<len; i++) {
+    int cnt = 0;
+    for (long long i=n; i!=0; i/=10) cnt++;
+    
+    int* answer = (int*)malloc(sizeof(int) * cnt);
+    
+    for (int i=0; i<cnt; i++) {
         answer[i] = n%10;
         n/=10;
     }
