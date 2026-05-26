822de82e6ac9fa689de6e113f669185bb3f54bae
kyungminLim <65112294+kyungmin-Earnest-Lim@users.noreply.github.com>
2026-05-26 18:57:34 +0900

    [level 2] Title: 올바른 괄호, Time: 0.31 ms, Memory: 4.37 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.c
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.c
@@ -1,20 +1,41 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define MAX 100000
+char stack[MAX+1];
+int top = -1;
+
+void push(const char c) {
+    stack[++top] = c;
+}
+
+char pop(void) {
+    return stack[top--];
+}
+
+void print_stack(void) {
+    for (int i=top; i>=0; i--) {
+        printf("[%c] ", stack[i]);
+    }
+}
+
+int is_empty(void) {
+    return top == -1;
+}
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 bool solution(const char* s) {
-    bool answer = true;
-    int balance = 0 ;
-    for (int i=0 ; s[i] != '\0' ; i++) {
-        if (s[i] == '(') {
-            balance++ ;
-        } 
+    top = -1;
+    int len = strlen(s);
+    
+    for (int i=0; i<len; i++) {
+        if (s[i] == '(') push(s[i]);
         else {
-            balance-- ;
-            
-            if (balance < 0) return false ;
+            if (is_empty()) return false;
+            pop();
         }
     }
-    return (balance == 0);
+    
+    return is_empty();
 }
