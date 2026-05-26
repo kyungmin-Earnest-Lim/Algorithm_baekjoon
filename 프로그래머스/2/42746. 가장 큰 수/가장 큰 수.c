ace068994cb0d62a0a382a3ab3eff347c944cbee
kyungminLim <65112294+kyungmin-Earnest-Lim@users.noreply.github.com>
2026-05-26 21:33:48 +0900

    [level 2] Title: 가장 큰 수, Time: 297.03 ms, Memory: 7.83 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.c
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.c
@@ -4,28 +4,23 @@
 #include <string.h>
 
 int compare(const void* a, const void* b) {
-    char ab[20];
-    char ba[20];
-    
+    char ab[10], ba[10];
     sprintf(ab, "%d%d", *(int*)a, *(int*)b);
     sprintf(ba, "%d%d", *(int*)b, *(int*)a);
-    
     return strcmp(ba, ab);
 }
 
 char* solution(int numbers[], size_t numbers_len) {
-    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     qsort(numbers, numbers_len, sizeof(int), compare);
-
     char* answer = (char*)malloc(4 * numbers_len + 1);
     answer[0] = '\0';
     
+    char temp[10];
     for (int i=0; i<numbers_len; i++) {
-        char temp[10];
         sprintf(temp, "%d", numbers[i]);
         strcat(answer, temp);
     }
-
+    
     if (answer[0] == '0') {
         answer[0] = '0';
         answer[1] = '\0';
