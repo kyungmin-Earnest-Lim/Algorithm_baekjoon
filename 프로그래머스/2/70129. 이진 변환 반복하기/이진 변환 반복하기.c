b003fcc11712a991253a0100add9a3b7018befd8
kyungminLim <65112294+kyungmin-Earnest-Lim@users.noreply.github.com>
2026-05-26 22:05:33 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.52 ms, Memory: 4.31 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.c
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.c
@@ -1,52 +1,46 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <string.h>
 
-// 10진수 → 2진수 문자열 변환
 void to_binary(int n, char* buf) {
     char temp[20];
-    int i = 0;
-    
-    while (n > 0) {
-        temp[i++] = (n % 2) + '0';
+    int i=0;
+    while (n>0) {
+        temp[i++] = n % 2 + '0';
         n /= 2;
     }
     temp[i] = '\0';
     
-    // 뒤집기
     int len = strlen(temp);
-    for (int j = 0; j < len; j++) {
+    
+    for (int j=0; j<len; j++) {
         buf[j] = temp[len - 1 - j];
     }
     buf[len] = '\0';
+    
 }
-
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int* solution(const char* s) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    
     int* answer = (int*)malloc(sizeof(int) * 2);
     answer[0] = 0; answer[1] = 0;
     
-    // s를 복사 (const라서 직접 수정 불가)
     char cur[150001];
     strcpy(cur, s);
     
     while (strcmp(cur, "1") != 0) {
-        
         int zero_count = 0;
-        int one_count  = 0;
+        int one_count = 0;
         
-        // 0, 1 개수 세기
-        for (int i = 0; cur[i] != '\0'; i++) {
-            if (cur[i] == '0') zero_count++;
-            else               one_count++;
+        for (int i=0; cur[i] != '\0'; i++) {
+            if(cur[i] == '1') one_count++;
+            else zero_count++;
         }
         
-        answer[0]++;              // 변환 횟수
-        answer[1] += zero_count;  // 제거된 0 누적
-        
-        // one_count → 2진수로 변환해서 cur 교체
+        answer[0]++;
+        answer[1] += zero_count;
         to_binary(one_count, cur);
     }
-    
     return answer;
 }
